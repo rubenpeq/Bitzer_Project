3cb9c766184c58aeabe0679563f890c5528c4832
rubenpeq <ruben1021@live.com>
2025-08-13 19:17:06 +0100

    Updated style and logo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { Navbar, Container } from "react-bootstrap";
-import logo from "../assets/bitzer_logo.png";
+import logo from "../assets/bitzer_logo.svg";
 import { Link } from "react-router-dom";
 
 export default function AppNavbar() {
   return (
-    <Navbar bg="light" className="justify-content-center py-3">
+    <Navbar className="justify-content-center py-3">
       <Container className="justify-content-center">
         <Navbar.Brand as={Link} to={"/"}>
           <img
